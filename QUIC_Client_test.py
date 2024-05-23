24ff505850acb113b14cc0d56e38d9285a4f2bd0
Ben The Winner <ben6119070@gmail.com>
2024-05-23 08:32:35 +0300

    I added a test function for the client

---
+++ QUIC_Client_test.py
@@ -0,0 +1,13 @@
+import unittesting
+
+import QUIC_Client
+from QUIC_Client import send_message
+
+class TestSendMessage(unittest.TestCase):
+    def test_send_message(self):
+        message = QUIC_Client.send_message("127.0.0.1", 12345)
+        self.assertEqual(message, "ivan")
+
+
+if __name__ == '__main__':
+    unittest.main()
